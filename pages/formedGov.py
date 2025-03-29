eaa5811a933b23917669b911dd1964f9d361fa95
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-03-30 10:53:21 +1100

    Update data 2025-03-30 10:53:21

--- pages/formedGov.py
+++ pages/formedGov.py
@@ -5,11 +5,21 @@ import plotly.express as px
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/formedGov.csv"
 
-st.title("Timeseries Dashboard")
+st.title("Likelihood of Forming Government")
 
 # Load CSV
 df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
 
+
+fig = px.line(
+    df,
+    y=["Labor", "Coalition"],
+    title="Election Trends",
+)
+
 # Display chart
-fig = px.line(df)
+#fig = px.line(df)
+fig.update_traces(selector=dict(name="Labor"), line=dict(color="red", dash="solid"))
+fig.update_traces(selector=dict(name="Coalition"), line=dict(color="blue", dash="solid"))
+
 st.plotly_chart(fig, use_container_width=True)
--- pages/govType.py
+++ pages/govType.py
@@ -5,11 +5,22 @@ import plotly.express as px
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/typeOfGov.csv"
 
-st.title("Timeseries Dashboard")
+st.title("Likely Type of Government")
 
 # Load CSV
 df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
 
+fig = px.line(
+    df,
+    y=["Coalition Majority", "Coalition Minority", "Labor Majority", "Labor Minority"],
+    title="Election Trends",
+)
+
 # Display chart
-fig = px.line(df)
+#fig = px.line(df)
+fig.update_traces(selector=dict(name="Labor Majority"), line=dict(color="red", dash="solid"))
+fig.update_traces(selector=dict(name="Coalition Majority"), line=dict(color="blue", dash="solid"))
+fig.update_traces(selector=dict(name="Labor Minority"), line=dict(color="red", dash="dash"))
+fig.update_traces(selector=dict(name="Coalition Minority"), line=dict(color="blue", dash="dash"))
+
 st.plotly_chart(fig, use_container_width=True)
