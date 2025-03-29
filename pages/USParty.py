4bf48164bc9a465ce4a80a1b4fb5efabd82b4e8f
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-03-30 10:47:25 +1100

    Update data 2025-03-30 10:47:25

---
+++ pages/USParty.py
@@ -0,0 +1,15 @@
+# streamlit_app.py
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/USParty.csv"
+
+st.title("Timeseries Dashboard")
+
+# Load CSV
+df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+
+# Display chart
+fig = px.line(df)
+st.plotly_chart(fig, use_container_width=True)
