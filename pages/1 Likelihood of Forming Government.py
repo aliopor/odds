4657b5b8d954009349a0f18d108a7c3653535a98
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-03-30 16:40:35 +1100

    Update data 2025-03-30 16:40:35

--- pages/1 Likelihood of Forming Government.py
+++ pages/1 Likelihood of Forming Government.py
@@ -10,7 +10,7 @@ import plotly.express as px
 #st.set_page_config(page_title="Likely Party to Form Government in 2024 Australian Election", layout="wide")
 
 # Optional: Repeat the title for clarity or styling
-#st.markdown("### Likely Party to Form Government in 2024 Australian Election")
+st.heading("### Likely Party to Form Government in 2024 Australian Election")
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/formedGov.csv"
 
