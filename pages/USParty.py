92e81396f118e657869e05e0fe02f79bd5dfc8b6
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-03-30 11:42:41 +1100

    Update data 2025-03-30 11:42:41

--- pages/USParty.py
+++ pages/USParty.py
@@ -1,11 +1,17 @@
+"""
+# 
+"""
+
 # streamlit_app.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-"""
-# US Election
-"""
+st.set_page_config(page_title="US Election", layout="wide")
+
+# Optional: Repeat the title for clarity or styling
+st.title("US Election")
+
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/USParty.csv"
 
