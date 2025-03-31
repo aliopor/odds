f2917b11ad02948012e5de6f58ff4ae9c46fd4e0
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-04-01 07:16:05 +1100

    Update data 2025-04-01 07:16:04

--- pages/1 Australian Election - Winning Party.py
+++ pages/1 Australian Election - Winning Party.py
@@ -44,6 +44,7 @@ csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/TwoPar
 rename_map = {'Labor 45 or Less (Coalition 55 or more)':"Labor 45% or less", 'Labor 46 - Coalition 54' : "Labor 46%", 'Labor 47 - Coalition 53' : "Labor 47%", 'Labor 48 - Coalition 52': "Labor 48%", 'Labor 49 - Coalition 51': "Labor 49%", 'Labor 50 - Coalition 50': "Labor 50%", 'Labor 51 - Coalition 49' : "Labor 51%", 'Labor 52 - Coalition 48' : "Labor 52%", 'Labor 53 - Coalition 47':"Labor 53%", 'Labor 54 - Coalition 46' : "Labor 54%", 'Labor 55 or More (Coalition 45 or less)': "Labor 55% or more"}
 
 df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+df = df*100
 scenarios = df.tail(1).columns.tolist()
 scenariosRenamed = [rename_map.get(item, item) for item in scenarios]
 values = df.iloc[-1].tolist()
