b1ca92ff057098733ff1d203ff79f5f76c64eed0
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-04-02 18:36:22 +1100

    Update data 2025-04-02 18:36:22

--- pages/4 US 2028 Election.py
+++ pages/4 US 2028 Election.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="2028 US Election Winning Party", layout="wide")
+st.set_page_config(page_title="2028 US Election", layout="wide")
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/USParty.csv"
 
@@ -42,4 +42,27 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.info("Source: [Presidential Election 2028 - Winning Party](https://www.sportsbet.com.au/betting/politics/us-politics/presidential-election-2028-winning-party-8732140)")
+csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/USElection.csv"
+
+
+# Load CSV
+df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+
+fig = px.line(
+    df,
+    title="Likely 2028 US Election Winner",
+)
+
+
+fig.update_layout(
+    yaxis=dict(
+        tickformat=".0%",
+        range=[0, 1]
+    ),
+    yaxis_title="Likelihood (%)",
+    xaxis_title="Date"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.info("Sources: [Presidential Election 2028 - Winning Party](https://www.sportsbet.com.au/betting/politics/us-politics/presidential-election-2028-winning-party-8732140) and [US Presidential Election 2028](https://www.sportsbet.com.au/betting/politics/us-politics/us-presidential-election-2028-8699063)")
