e0efa06f3379e9ec2b3cd3ce1a82d3ec36ea2564
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-04-02 19:11:40 +1100

    Update data 2025-04-02 19:11:40

--- pages/2 Australian Election - Type of Government.py
+++ pages/2 Australian Election - Type of Government.py
@@ -40,4 +40,33 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.info("Source: [Type of Government Formed](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/type-of-government-formed-8878095)")
+csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/Majority.csv"
+
+
+
+# Load CSV
+df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+
+fig = px.line(
+    df,
+    y=["Yes"],
+    title="Chance of a Majority Government in 2024 Australian Election",
+)
+
+# Display chart
+#fig = px.line(df)
+fig.update_traces(selector=dict(name="Yes"), line=dict(color="red", dash="solid"))
+
+
+fig.update_layout(
+    yaxis=dict(
+        tickformat=".0%",
+        range=[0, 1]
+    ),
+    yaxis_title="Likelihood (%)",
+    xaxis_title="Date"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.info("Sources: [Type of Government Formed](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/type-of-government-formed-8878095) [Majority Government](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/majority-government-8878114)")
