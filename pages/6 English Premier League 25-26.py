4f1e4f71dbbf84fbd90534a6f42da14f1a4d1a88
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-04-29 08:48:08 +1000

    Update data 2025-04-29 08:48:08

--- pages/6 English Premier League 25-26.py
+++ pages/6 English Premier League 25-26.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="2025 NBA Winner", layout="wide")
+st.set_page_config(page_title="2025/2026 EPL Winner", layout="wide")
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/EPL.csv"
 
@@ -14,7 +14,7 @@ df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
 
 fig = px.line(
     df,
-    title="Likely 2024/2025 English Premier League Winner"
+    title="Likely 2025/2026 English Premier League Winner"
 )
 
 
@@ -30,4 +30,4 @@ fig.update_layout(
 st.plotly_chart(fig, use_container_width=True)
 
 
-st.info("Source: [English Premier League Outrights 2024/25](https://www.sportsbet.com.au/betting/soccer/united-kingdom/english-premier-league/english-premier-league-outrights-2024-25-8256992)")
+st.info("Source: [English Premier League Outrights 2025/26](https://www.sportsbet.com.au/betting/soccer/united-kingdom/english-premier-league/english-premier-league-outrights-2025-26-9166519)")
