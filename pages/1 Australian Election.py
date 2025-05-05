c677861b6ea1f70ffc546b746391421554535e72
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-05-05 18:44:40 +1000

    Update data 2025-05-05 18:44:40

---
+++ pages/1 Australian Election.py
@@ -0,0 +1,42 @@
+"""
+# Likelihood of Forming Government
+"""
+
+# streamlit_app.py
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go 
+
+st.set_page_config(page_title="Likely Party to Form Government in 2028(?) Australian Election", layout="wide")
+
+csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/formedGov.csv"
+
+# Load CSV
+df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+
+
+
+fig = px.line(
+    df,
+    y=["Labor", "Coalition"],
+    title="Likely Party to Form Government in 2028(?) Australian Election"
+)
+
+# Display chart
+#fig = px.line(df)
+fig.update_traces(selector=dict(name="Labor"), line=dict(color="red", dash="solid"))
+fig.update_traces(selector=dict(name="Coalition"), line=dict(color="blue", dash="solid"))
+
+fig.update_layout(
+    yaxis=dict(
+        tickformat=".0%",
+        range=[0, 1]
+    ),
+    yaxis_title="Likelihood (%)",
+    xaxis_title="Date"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.info("Source: [48th Parliament of Australia](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/48th-parliament-of-australia-8571604) and [Two-Party Preferred Vote Percentage](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/two-party-preferred-vote-percentage-9083891)")
