5aa64eb13cbf05a094f2fdede06bf4ec989df6b4
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2025-06-09 10:49:10 +1000

    Update data 2025-06-09 10:49:10

--- pages/4 Formula 1 2025.py
+++ pages/4 Formula 1 2025.py
@@ -32,28 +32,5 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/F1Constructors.csv"
 
-
-# Load CSV
-df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
-
-fig = px.line(
-    df,
-    title="Likely 2025 F1 Constructors Champion",
-)
-
-
-fig.update_layout(
-    yaxis=dict(
-        tickformat=".0%",
-        range=[0, 1]
-    ),
-    yaxis_title="Likelihood (%)",
-    xaxis_title="Date"
-)
-
-st.plotly_chart(fig, use_container_width=True)
-
-
-st.info("Sources: [F1 Drivers Championship 2025](https://www.sportsbet.com.au/betting/motor-racing/formula-1/f1-drivers-championship-2025-8767626) and [F1 Constructors Championship 2025](https://www.sportsbet.com.au/betting/motor-racing/formula-1/f1-constructors-championship-2025-8808443)")
+st.info("Source: [F1 Drivers Championship 2025](https://www.sportsbet.com.au/betting/motor-racing/formula-1/f1-drivers-championship-2025-8767626)")
