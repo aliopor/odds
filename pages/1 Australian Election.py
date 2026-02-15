5bb0b8466843f53e3022a96766ad1a21df55b9a0
Ranmadhu Wijayatilaka <ranmadhu@fileserver.akamad.com>
2026-02-15 20:36:59 +1100

    Update data 2026-02-15 20:36:59

--- pages/1 Australian Election.py
+++ pages/1 Australian Election.py
@@ -9,6 +9,31 @@ import plotly.express as px
 import plotly.graph_objects as go 
 
 st.set_page_config(page_title="Likely Party to Form Government in 2028(?) Australian Election", layout="wide")
+##########################################
+csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/2026FarrerByElection.csv"
+
+
+# Load CSV
+df = pd.read_csv(csv_url, parse_dates=["tstamp"], index_col="tstamp")
+
+fig = px.line(
+    df,
+    title="Likely winner of the Farrer By-Election
+)
+
+
+fig.update_layout(
+    yaxis=dict(
+        tickformat=".0%",
+        range=[0, 1]
+    ),
+    yaxis_title="Likelihood (%)",
+    xaxis_title="Date"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+#####################################
 
 csv_url = "https://raw.githubusercontent.com/aliopor/odds/refs/heads/main/formedGov.csv"
 
@@ -43,4 +68,4 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.info("Source: [49th Parliament of Australia](https://www.sportsbet.com.au/betting/politics/australian-federal-politics/49th-parliament-of-australia-9186904)")
+st.info("Source: [Australian Federal Politics](https://www.sportsbet.com.au/betting/politics/australian-federal-politics.)")
